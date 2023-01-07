0d69fb6418c4f543a36dc4be051bd3a90355935b
Manoel Lucio <manoellucio@Manoels-MacBook-Air.local>
2023-01-07 14:55:30 -0300

    criado o header da ToDo App

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,24 @@
+
+import Logo from '../../assets/Logo.svg'
+
+import styles from './header.module.css'
+
+import {AiOutlinePlusCircle} from 'react-icons/ai'
+
+export function Header(){
+    return (
+        <header className={styles.header}>
+            <img src={Logo} />
+
+            <form className={styles.newTaskForm}>
+                <input 
+                    placeholder='Adicione uma nova tarefa'
+                />
+                <button>
+                    Criar
+                    <AiOutlinePlusCircle size={20}/>
+                </button>
+            </form>
+        </header>
+    )
+}
